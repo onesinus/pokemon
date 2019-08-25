533d73abab5f2ed5540cb3afa91548ba90bc7c2f
Onesinus SPT <onesinus231@gmail.com>
2019-08-25 15:58:25 +0700

    Test header dan menu load

--- __tests__/__snapshots__/App.test.js
+++ __tests__/__snapshots__/App.test.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Main from '../../src/./Main';
 import { shallow } from '../.././enzyme';
+import { NavLink } from "react-router-dom";
 
-test('Logo berhasil dirender', () => {
+
+test('Header pokemon berhasil diload [Logo, List Menu]', () => {
 	const app = shallow(<Main/>);
 	expect(app.containsAnyMatchingElements([
 		<img id="logo" src="logo.png" alt="Logo not loaded"/>
+	])
+  ).toBe(true);
+
+	expect(app.containsAnyMatchingElements([
+		<li><NavLink to="/my_pokemon">My Pokemon</NavLink></li>
+	])
+  ).toBe(true);
+
+	expect(app.containsAnyMatchingElements([
+		<li><NavLink to="/">Pokemon List</NavLink></li>
 	])
   ).toBe(true);
 });
