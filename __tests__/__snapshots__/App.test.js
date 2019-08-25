5dc002412ca489345c9ba28f901206c10c8bf392
Onesinus SPT <onesinus231@gmail.com>
2019-08-25 15:53:53 +0700

    Coba unit testing menggunakan enzyme

--- __tests__/__snapshots__/App.test.js
+++ __tests__/__snapshots__/App.test.js
@@ -1,3 +1,11 @@
-test('Try Test', () => {
-	expect(true).toBeTruthy();
-})
+import React from 'react';
+import Main from '../../src/./Main';
+import { shallow } from '../.././enzyme';
+
+test('Logo berhasil dirender', () => {
+	const app = shallow(<Main/>);
+	expect(app.containsAnyMatchingElements([
+		<img id="logo" src="logo.png" alt="Logo not loaded"/>
+	])
+  ).toBe(true);
+});
